2e134d6be34cfda8a2e9ab3b857e442ec4c01573
Sagar RS <sagarrs@Sagars-MacBook-Pro.local>
2022-05-30 14:26:42 +0530

    navbar 1.0

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,14 @@
 import "./Home.scss"
 import {Sidebar} from '../Sidebar/Sidebar'
+import { Navbar } from "../Navbar/Navbar"
 
 export const Home = () => {
     return(
         <div className="home">
             <Sidebar/>
-            <div className="homeContainer">container</div>
+            <div className="homeContainer">
+                <Navbar/>
+            </div>
         </div>
     )
 }
