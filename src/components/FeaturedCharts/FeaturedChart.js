bf268f19a1b6046db6902a75dfd7029c4638eb8e
Sagar RS <sagarrs@Sagars-MacBook-Pro.local>
2022-05-31 21:54:08 +0530

    Featured 2.0

--- src/components/FeaturedCharts/FeaturedChart.js
+++ src/components/FeaturedCharts/FeaturedChart.js
@@ -2,6 +2,8 @@ import "./FeaturedChart.scss"
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import MoreVertIcon from '@mui/icons-material/MoreVert'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined'
 
 export const FeaturedChart = () => {
     return(
@@ -12,8 +14,38 @@ export const FeaturedChart = () => {
             </div>
             <div className="bottom">
                 <div className="featuredChart">
-                    <CircularProgressbar value={70} text={`70%`} />
+                    <CircularProgressbar value={70} text={`70%`} strokeWidth={5} />
                 </div>
+                <p className="title">Total sales made today</p>
+                <p className="amount">$420</p>
+                <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                <div className="summary">
+                    <div className="item">
+                        <div className="itemTitle">Target</div>
+                        <div className="itemResult positive">
+                            <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+                            <div className="resultAmount">$12.4k</div>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="itemTitle">Last week</div>
+                        <div className="itemResult negative">
+                            <KeyboardArrowDownIcon fontSize="small"/>
+                            <div className="resultAmount">-$12.4k</div>
+                        </div>
+                    </div>
+
+                    <div className="item">
+                        <div className="itemTitle">Last month</div>
+                        <div className="itemResult positive">
+                            <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+                            <div className="resultAmount">$12.4k</div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
