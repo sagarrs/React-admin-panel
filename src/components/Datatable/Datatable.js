67f980f85da1481ae986a4c4e71d47c07f854964
Sagar RS <sagarrs@Sagars-MacBook-Pro.local>
2022-06-05 17:44:33 +0530

    sidebar links added

--- src/components/Datatable/Datatable.js
+++ src/components/Datatable/Datatable.js
@@ -1,6 +1,7 @@
 import "./Datatable.scss"
 import { DataGrid } from '@mui/x-data-grid';
 import {userColumns, userRows} from '../../DatatableSource'
+import {Link} from "react-router-dom"
 
 export const Datatable = () => {
 
@@ -12,9 +13,11 @@ export const Datatable = () => {
             renderCell:()=>{
                 return(
                     <div className="cellAction">
-                        <div className="viewButton">
-                            View
-                        </div>
+                        <Link to="/users/test" style={{textDecoration: "none"}}>
+                            <div className="viewButton">
+                                View
+                            </div>
+                        </Link>
                         <div className="deleteButton">
                             Delete
                         </div>
@@ -25,6 +28,12 @@ export const Datatable = () => {
     ]
     return(
         <div className="datatable">
+            <div className="datatableTitle">
+                Add new user
+                <Link to="/users/new" className="link">
+                    Add New
+                </Link>
+            </div>
             <DataGrid
                 rows={userRows}
                 columns={userColumns.concat(actionColumn)}
